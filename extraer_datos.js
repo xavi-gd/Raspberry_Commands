6119e4c9af569c125c24c8dd650d4aeca3132b2c
xavi-gd <80868670+xavi-gd@users.noreply.github.com>
2022-12-30 18:37:59 +0100

    sin el import-export todo en un fichero

--- extraer_datos.js
+++ extraer_datos.js
@@ -186,26 +186,35 @@ for (let module = 0; module < modules_values.length; module++) {
         modules_values[module][value] = (modules_values[module][value] + offset_values[value]) * scale_factor_values[value]
     }
 }
-function Module(id, module_values) {
-    const VOLT = 0, CURRENT = 1, REMAIN_CAPACITY = 2, AVG_TEMP_CELL = 3, ENV_TEMP = 4, SOC = 8, SOH = 10, PCB_TEMP = 11
-    const FIRST_CELL_VOLT = 14, LAST_CELL_VOLT = 29, FIRST_CELL_TEMP = 31, LAST_CELL_TEMP = 46
-    const FULL_CAPACITY = 47, CHARGE_TIME = 48, DISCHARGE_TIME = 49
-    this.id = id
-    this.volt = module_values[VOLT]
-    this.current = module_values[CURRENT]
-    this.remain_capacity = module_values[REMAIN_CAPACITY]
-    this.avg_cell_temp = module_values[AVG_TEMP_CELL]
-    this.env_temp = module_values[ENV_TEMP]
-    this.soc = module_values[SOC]
-    this.soh = module_values[SOH]
-    this.pcb_temp = module_values[PCB_TEMP]
-    this.cell_volt = module_values.slice(FIRST_CELL_VOLT, LAST_CELL_VOLT + 1)
-    this.cell_temp = module_values.slice(FIRST_CELL_TEMP, LAST_CELL_TEMP + 1)
-    this.full_capacity = module_values[FULL_CAPACITY]
-    this.charge_time = module_values[CHARGE_TIME]
-    this.discahrge_time = module_values[DISCHARGE_TIME]
+
+/**
+ * @param {number} id
+ * @param {number[]} module_values
+ */
+class Module {
+    constructor(id, module_values) {
+        const VOLT = 0, CURRENT = 1, REMAIN_CAPACITY = 2, AVG_TEMP_CELL = 3, ENV_TEMP = 4, SOC = 8, SOH = 10,
+            PCB_TEMP = 11
+        const FIRST_CELL_VOLT = 14, LAST_CELL_VOLT = 29, FIRST_CELL_TEMP = 31, LAST_CELL_TEMP = 46
+        const FULL_CAPACITY = 47, CHARGE_TIME = 48, DISCHARGE_TIME = 49
+        this.id = id
+        this.volt = module_values[VOLT]
+        this.current = module_values[CURRENT]
+        this.remain_capacity = module_values[REMAIN_CAPACITY]
+        this.avg_cell_temp = module_values[AVG_TEMP_CELL]
+        this.env_temp = module_values[ENV_TEMP]
+        this.soc = module_values[SOC]
+        this.soh = module_values[SOH]
+        this.pcb_temp = module_values[PCB_TEMP]
+        this.cell_volt = module_values.slice(FIRST_CELL_VOLT, LAST_CELL_VOLT + 1)
+        this.cell_temp = module_values.slice(FIRST_CELL_TEMP, LAST_CELL_TEMP + 1)
+        this.full_capacity = module_values[FULL_CAPACITY]
+        this.charge_time = module_values[CHARGE_TIME]
+        this.discahrge_time = module_values[DISCHARGE_TIME]
+    }
 }
-const modules = new Array(3)
+
+const modules = new Array(active_modules.length)
 for (let module = 0; module < modules_values.length; module++) {
     modules[module] = new Module(active_modules[module], modules_values[module])
 }
